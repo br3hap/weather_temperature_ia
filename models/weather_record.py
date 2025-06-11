c837b232a33d51550816cb80231e60fb7f7d09f6
Breithner <Breithnerap@gmail.com>
2025-06-11 11:41:04 -0500

    upload change

--- models/weather_record.py
+++ models/weather_record.py
@@ -11,4 +11,13 @@ class WeatherRecord(models.Model):
     _name = 'weather.record'
     _description = 'Registro de temperatura por día'
 
-    name = fields.Char('Name')
+    # name = fields.Char('Name')
+    city_id = fields.Many2one('weather.city', string="Ciudad", required=True, ondelete='cascade')
+    date = fields.Date(string="Fecha", default=fields.Date.today, required=True)
+    max_temperature = fields.Float(string="Temp. Máxima")
+    min_temperature = fields.Float(string="Temp. Mínima")
+    predicted = fields.Boolean(string="Predicción por IA", default=False)
+
+    _sql_constraints = [
+        ('unique_city_date', 'unique(city_id, date)', 'Ya existe un registro para esta ciudad.')
+    ]
