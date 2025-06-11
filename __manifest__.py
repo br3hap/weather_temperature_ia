207d212f7e54a1ba77c04657fa183ffcdadf290a
Breithner <Breithnerap@gmail.com>
2025-06-11 11:26:00 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Temperatura Min/Max',
+    'version': '1.0',
+    'summary': """ Módulo que permite ubicar la temperatura mínima y máxima de las ciudades del Perú """,
+    'author': 'Breithner Aquituari',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
